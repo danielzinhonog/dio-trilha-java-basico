9a2df649eff9bd026aad15728670b40214953a99
danielzinhonog <danielcorreanogueira@outlook.com>
2025-04-10 17:40:52 -0300

    Começando a matérria de Pilares de POO

---
+++ pilares-poo/src/MSNMenssenger.java
@@ -0,0 +1,14 @@
+public class MSNMenssenger{
+    public void enviarMensagem(){
+        System.out.println("Enviando mensagem");
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem");
+    }
+    public void validarConectadoInternet(){
+        System.out.println("Validando se está à internet");
+    }
+    public void salvarHistoricoMensagem(){
+        System.out.println("Salvando histórico de mensagens");
+    }
+}
