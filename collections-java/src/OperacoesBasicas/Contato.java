2f7c8e3e64b003959c5200bd98e4dc8bb2725185
danielzinhonog <danielcorreanogueira@outlook.com>
2025-04-16 15:13:08 -0300

    Seguindo com a matéria de Collections Java

---
+++ collections-java/src/OperacoesBasicas/Contato.java
@@ -0,0 +1,30 @@
+package OperacoesBasicas;
+
+import java.util.Objects;
+
+public class Contato{
+    private String nome;
+    private int numero;
+    
+    public Contato(String nome, int numero){
+        this.nome = nome;
+        this.numero = numero;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getNumero(){
+        return numero;
+    }
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Contato contato)) return false;
+        return Objects.equals(getNome(), contato.getNome());
+    }
+    public String toString(){
+        return "{" + nome + ',' + numero + "}";
+    }
+}
