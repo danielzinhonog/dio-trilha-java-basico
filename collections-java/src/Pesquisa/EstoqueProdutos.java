5f1f09ec46d0e78abb1f3ac054eb2409cec2cf2e
danielzinhonog <danielcorreanogueira@outlook.com>
2025-04-22 15:19:48 -0300

    Terminada a matéria de Collections Java

---
+++ collections-java/src/Pesquisa/EstoqueProdutos.java
@@ -0,0 +1,54 @@
+package Pesquisa;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class EstoqueProdutos{
+    //atributos
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos(){
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco){
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+    public void exibirProdutos(){
+        System.out.println(estoqueProdutosMap);
+    }
+    public double calcularValorTotalEstoque(){
+        double valorTotalEstoque = 0d;
+        if(!estoqueProdutosMap.isEmpty()){
+            for(Produto p : estoqueProdutosMap.values()){
+                valorTotalEstoque += p.getQuantidade() + p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if(!estoqueProdutosMap.isEmpty()){
+            for(Produto p : estoqueProdutosMap.values()){
+                if(p.getPreco() > maiorPreco){
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        estoque.exibirProdutos();
+
+        estoque.adicionarProduto(1L, "Produto A", 10, 5);
+        estoque.adicionarProduto(2L, "Produto B", 5, 10);
+        estoque.adicionarProduto(3L, "Produto C", 2, 15);
+
+        estoque.exibirProdutos();
+
+        System.out.println("Valor do estoque: R$" + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais caro: " + estoque.obterProdutoMaisCaro());
+    }
+}
